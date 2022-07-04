23adc1d5fa38a14a3030865827cb87a468c625a5
Shane Reynolds <x21126143@student.ncirl.ie>
2022-07-04 22:58:19 +0100

    Refactoring to employee + Update/Delete requests

--- src/main/java/io/shanereynolds/springbootwebapp/employees/EmployeeController.java
+++ src/main/java/io/shanereynolds/springbootwebapp/employees/EmployeeController.java
@@ -5,23 +5,44 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class EmployeeController {
 	
 	@Autowired
-	private EmployeeService topicService;
+	private EmployeeService employeeService;
 	
-	@RequestMapping("topics")
+	@RequestMapping("employees")
 	public List<Employee> getAllTopics() {
-		return topicService.getAllTopics();
+		return employeeService.getAllTopics();
 	}
 	
-	@RequestMapping("/topics/{foo}")
-	public Employee getTopic(@PathVariable("foo") String id) {
-		return topicService.getEmployee(id);
+	@RequestMapping("/employees/{email}")
+	public Employee getTopic(@PathVariable String email) {
+		return employeeService.getEmployee(email);
 	}
+	
+	@PostMapping(value="/employees")
+	public void addEmployee(@RequestBody Employee employee) {
+		employeeService.addEmployee(employee);
+	}
+	
+	@RequestMapping(method=RequestMethod.PUT, value="/employees/{email}")
+	public void updateEmployee(@RequestBody Employee employee, @PathVariable String email) {
+		employeeService.updateEmployee(email, employee);
+	}
+	
+	@RequestMapping(method=RequestMethod.DELETE, value="/employees/{email}")
+	public Employee deleteTopic(@PathVariable String email) {
+		return employeeService.deleteEmployee(email);
+	}
+	
+	
 
 }
--- src/main/java/io/shanereynolds/springbootwebapp/employees/EmployeeService.java
+++ src/main/java/io/shanereynolds/springbootwebapp/employees/EmployeeService.java
@@ -1,5 +1,6 @@
 package io.shanereynolds.springbootwebapp.employees;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -8,18 +9,38 @@ import org.springframework.stereotype.Service;
 @Service
 public class EmployeeService {
 	
-	List<Employee> topics = Arrays.asList(
-			new Employee("JohnS336@gmail.com", "cherries22", "John Smith"),
-			new Employee("BethHenderson@hotmail.com", "pineapple58", "Beth Henderson"),
+	static List<Employee> employees = new ArrayList<>(Arrays.asList(
+			new Employee("johns336@gmail.com", "cherries22", "John Smith"),
+			new Employee("bethhenderson@hotmail.com", "pineapple58", "Beth Henderson"),
 			new Employee("tcrimson@outlook.ie", "apples72", "Timothy Crimson")
-			);
+			));
 	
 	public List<Employee> getAllTopics() {
-		return topics;
+		return employees;
 	}
 	
 	public Employee getEmployee(String employeeEmail) {
-		return topics.stream().filter(t -> t.getEmployeeEmail().equals(employeeEmail)).findFirst().get();
+		return employees.stream().filter(t -> t.getEmployeeEmail().equals(employeeEmail)).findFirst().get();
+	}
+
+	public void addEmployee(Employee employee) {	
+		employees.add(employee);
+	}
+
+	public void updateEmployee(String email, Employee employee) {
+		for(int i = 0; i < employees.size(); i++) {
+			Employee e = employees.get(i);
+			if(e.getEmployeeEmail().equals(email)) {
+				employees.set(i, employee);
+				return;
+			}
+		}
+		
+	}
+
+	public Employee deleteEmployee(String email) {
+		employees.removeIf(e -> e.getEmployeeEmail().equals(email));
+		return null;
 	}
 	
 
