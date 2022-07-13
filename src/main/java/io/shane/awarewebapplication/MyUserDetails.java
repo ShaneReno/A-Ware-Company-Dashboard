73c4975395b6cdeaf9541e055e4809c9bb41d95e
Shane Reynolds <x21126143@student.ncirl.ie>
2022-07-13 19:28:12 +0100

    First login implementation using Spring security

---
+++ src/main/java/io/shane/awarewebapplication/MyUserDetails.java
@@ -0,0 +1,62 @@
+package io.shane.awarewebapplication;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class MyUserDetails implements UserDetails{
+	
+	
+	
+	//Testing hard coded constructor
+	private String userName;
+
+	public MyUserDetails(String userName) {
+		this.userName = userName;
+	}
+	
+	public MyUserDetails() {
+	}
+
+	
+	//Generated methods for authentication of user details
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return Arrays.asList(new SimpleGrantedAuthority("ROLE_USER"));
+	}
+
+	@Override
+	public String getPassword() {
+		return "pass";
+	}
+
+	@Override
+	public String getUsername() {
+		return userName;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
+	
+
+}
