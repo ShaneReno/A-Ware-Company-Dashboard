dc2c999fe7a81250503e32bcd9853c0775b14060
Shane Reynolds <x21126143@student.ncirl.ie>
2022-07-19 02:01:20 +0100

    Re ordering the SQL so that it is compatible with spring security

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+insert into users values ('sreynolds', 'cherries51', true);	
+insert into users values ('ncraigie0@ezinearticles.com', 'YNlx7SHy', true);
+insert into authorities values ('sreynolds', 'ROLE_ADMIN');
+insert into authorities values ('ncraigie0@ezinearticles.com', 'ROLE_USER');
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,15 +1,8 @@
-CREATE DATABASE IF NOT EXISTS aware;
-
-USE aware; #Created Schema
-
-
 CREATE TABLE users (username varchar(50) not null primary key, password varchar(50) not null, enabled boolean not null);
-insert into users values ('user', 'pass', true);
-insert into users values ('admin', 'pass', true);	
+
 
 CREATE TABLE authorities (username varchar(50) not null, authority varchar(50) not null, constraint fk_authorities_users foreign key(username) references users(username));
-insert into authorities values ('user', 'ROLE_USER');
-insert into authorities values ('admin', 'ROLE_ADMIN');
+
 
 CREATE unique index ix_auth_username on authorities (username,authority);
 
